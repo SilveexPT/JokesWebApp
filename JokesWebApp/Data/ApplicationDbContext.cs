55c9c464ed3d440195d7337873df7d3e8ccdb3fc
7170 <7170@E306PC01>
2023-11-10 16:10:11 +0000

    Adicionar arquivos de projeto.

---
+++ JokesWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jOKESWEBAPP.Models;
+
+namespace jOKESWEBAPP.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jOKESWEBAPP.Models.Jokes> Jokes { get; set; } = default!;
+    }
+}
